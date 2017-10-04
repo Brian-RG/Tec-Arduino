3a242439e998b47045b717a365b4485fdf557b3a
Brian R <brianreyesg@hotmail.com>
2017-10-04 10:06:35 -0500

    Add files via upload

---
+++ Practica4.ino
@@ -0,0 +1,41 @@
+long tiempo;
+int disparador = 2;
+int entrada = 4;
+float distancia;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(disparador,OUTPUT);
+  pinMode(entrada,INPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  delay(1000);
+  digitalWrite(disparador, HIGH);
+  delay (30);
+  digitalWrite(disparador, LOW);
+
+  tiempo = (pulseIn(entrada, HIGH)/2);
+
+  distancia = float(tiempo * 0.0343);
+  Serial.print(distancia);
+  Serial.println("cm");
+
+  if(distancia <10){
+      digitalWrite(8,HIGH);
+    }
+  else if(distancia >10 and distancia <20){
+      digitalWrite(9,HIGH);
+    }
+  else if (distancia >20){
+      digitalWrite(10,HIGH);
+    }
+  }
+  
+
